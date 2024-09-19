b417c3a4879f32da6d9da25dbd774331acf11792
Saloni Sharma <saloni.d.sharma@gmail.com>
2024-09-19 13:58:01 -0500

    update 3
    .next to gitignore

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -9,11 +9,6 @@ self.__BUILD_MANIFEST = {
   "lowPriorityFiles": [],
   "rootMainFiles": [],
   "pages": {
-    "/": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/index.js"
-    ],
     "/_app": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
@@ -24,10 +19,15 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
-    "/event-management": [
+    "/history": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
-      "static/chunks/pages/event-management.js"
+      "static/chunks/pages/history.js"
+    ],
+    "/notifications": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/notifications.js"
     ]
   },
   "ampFirstPages": []
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "27d33682b2edd2d8"; }
+/******/ 		__webpack_require__.h = function() { return "5ac9cc9ba63ce784"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
